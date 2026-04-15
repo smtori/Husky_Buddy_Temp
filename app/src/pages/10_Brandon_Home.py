1c29d0d050ae18fdc0b04b9a4d101f1c5ce4b7ea
Tori Smith <smithmtori@gmail.com>
2026-04-15 15:30:42 -0400

    gitMerge branch 'main' of https://github.com/smtori/Husky_Buddy_Temp

--- app/src/pages/10_Brandon_Home.py
+++ app/src/pages/10_Brandon_Home.py
@@ -9,10 +9,10 @@ st.set_page_config(layout='wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
+st.title(f"Welcome, {st.session_state['first_name']}.")
 st.write('### What would you like to do today?')
 
-if st.button('View NGO Directory',
+if st.button('Edit Profile',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/14_NGO_Directory.py')
