a8f4a5a6f69b4345850dc135d0873421e993ce22
Tori Smith <smithmtori@gmail.com>
2026-04-18 21:13:33 -0400

    change Brandon button names

--- app/src/pages/10_Brandon_Home.py
+++ app/src/pages/10_Brandon_Home.py
@@ -17,12 +17,12 @@ if st.button('Edit Profile',
              use_container_width=True):
     st.switch_page('pages/14_NGO_Directory.py')
 
-if st.button('Add New NGO',
+if st.button('View HuskyBuddy Chat',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/15_Add_NGO.py')
 
-if st.button('Predict Value Based on Regression Model',
+if st.button('Submit a Report',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/11_Prediction.py')
