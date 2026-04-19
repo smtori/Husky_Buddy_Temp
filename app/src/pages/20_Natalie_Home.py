73f6699e7a95f3a0f62debc7e75ece6ccc54059b
Tori Smith <smithmtori@gmail.com>
2026-04-18 22:08:48 -0400

    add edit profile to natalie persona

--- app/src/pages/20_Natalie_Home.py
+++ app/src/pages/20_Natalie_Home.py
@@ -11,7 +11,7 @@ SideBarLinks()
 st.title('System Admin Home Page')
 st.write('### What would you like to do today?')
 
-if st.button('Update ML Models',
+if st.button('Edit Profile',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/21_ML_Model_Mgmt.py')
+    st.switch_page('pages/15_Edit_Profile.py')
